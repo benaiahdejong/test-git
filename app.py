9987a4f006c9e52a8e1a04f080ee92db130f9dce
Benaiah de Jong <benaiahdejong@gmail.com>
2022-10-24 12:51:31 +0200

    *

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Im online!!!"
+    return "I'm online!!!"
 if __name__ == '__main__':
     app.run(debug=True)
